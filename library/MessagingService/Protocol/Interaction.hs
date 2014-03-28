58d3baff402ab271017b40a5c9b75556ba79f4af
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-03-28 14:36:19 +0400

    Replace server's Session monad with a function

--- library/MessagingService/Protocol/Interaction.hs
+++ library/MessagingService/Protocol/Interaction.hs
@@ -1,7 +1,6 @@
 module MessagingService.Protocol.Interaction where
 
-import MessagingService.Util.Prelude hiding (State)
-import qualified MessagingService.SessionT as SessionT; import MessagingService.SessionT (SessionT)
+import MessagingService.Util.Prelude
 
 
 -- Protocol
@@ -30,57 +29,3 @@ data FailureResponse =
   deriving (Show, Generic)
 
 instance Serializable m FailureResponse
-
-
--- Server-side
------------------------------
-
--- | 
--- A function which processes requests of type @i@ from client and 
--- produces a response of type @o@,
--- while maintaining a user-defined session state of type @s@ per each client.
--- 
--- This function essentially is what defines what the server actually does.
-type ProcessUserRequest i o s = State s -> i -> IO o
-
--- |
--- A mutable state associated with particular client's connection.
--- Since we're in `IO` anyway, we use a mutable state with `IORef` wrapper.
--- You're free to extend it with whatever the data structure you want.
-type State s = IORef (Maybe s)
-
-runServerSide :: 
-  forall i o s m. 
-  (MonadIO m, Serializable IO i, Serializable IO o, Applicative m) =>
-  ProcessUserRequest i o s -> SessionT m ()
-runServerSide processRequest = do
-  state <- liftIO $ newIORef Nothing
-  let 
-    loop = do
-      i <- catchError receive $ \e -> do
-        case e of
-          SessionT.TimeoutReached -> send $ Left $ TimeoutReached
-          SessionT.CorruptData t -> send $ Left $ CorruptRequest t
-          _ -> return ()
-        throwError e
-      case i of
-        CloseSession -> do
-          send $ Right $ Nothing
-        Keepalive -> do
-          send $ Right $ Nothing
-          loop
-        UserRequest a -> do
-          o <- liftIO $ processRequest state a
-          send $ Right $ Just o
-          loop
-  loop
-  where
-    receive = SessionT.receive :: SessionT m (Request i)
-    send = SessionT.send :: Response o -> SessionT m ()
-
-
--- Client-side
------------------------------
-
--- runClientSide :: Request i -> SessionT m (Response o)
--- runClientSide
--- library/MessagingService/Server/Session.hs
+++ library/MessagingService/Server/Session.hs
@@ -1,22 +1,18 @@
 module MessagingService.Server.Session where
 
 import MessagingService.Util.Prelude hiding (State, listen, interact)
-import qualified MessagingService.SessionT as SesisonT
-import qualified MessagingService.Protocol.Interaction as Interaction
+import MessagingService.Protocol.Interaction 
+import qualified MessagingService.SessionT as SessionT; import MessagingService.SessionT (SessionT)
 
 
--- | 
--- A user session on server.
-newtype Session i o s r = 
-  Session (ReaderT (ProcessMessageWithState i o) (SesisonT.SessionT IO) r)
-  deriving (Functor, Applicative, Monad, MonadIO, MonadError SesisonT.Failure)
 
 -- | 
--- A function which processes messages from client (@i@) and produces a response (@o@),
--- while maintaining a user-defined session state (@s@) per each client.
+-- A function which processes requests of type @i@ from client and 
+-- produces a response of type @o@,
+-- while maintaining a user-defined session state of type @s@ per each client.
 -- 
--- This function essentially is what defines what your server actually does.
-type ProcessMessage i o s = State s -> i -> IO o
+-- This function essentially is what defines what the server actually does.
+type ProcessUserRequest i o s = State s -> i -> IO o
 
 -- |
 -- A mutable state associated with particular client's connection.
@@ -24,37 +20,32 @@ type ProcessMessage i o s = State s -> i -> IO o
 -- You're free to extend it with whatever the data structure you want.
 type State s = IORef (Maybe s)
 
-type ProcessMessageWithState i o = i -> IO o
-
-run :: 
-  (SesisonT.Settings, ProcessMessage i o s) ->
-  Session i o s r -> IO (Either SesisonT.Failure r)
-run (connectionSettings, processMessage) (Session t) = do
+interact :: 
+  forall i o s m. 
+  (MonadIO m, Serializable IO i, Serializable IO o, Applicative m) =>
+  ProcessUserRequest i o s -> SessionT m ()
+interact processRequest = do
   state <- liftIO $ newIORef Nothing
-  runReaderT t (processMessage state) |> flip SesisonT.run connectionSettings
+  let 
+    loop = do
+      i <- catchError receive $ \e -> do
+        case e of
+          SessionT.TimeoutReached -> send $ Left $ TimeoutReached
+          SessionT.CorruptData t -> send $ Left $ CorruptRequest t
+          _ -> return ()
+        throwError e
+      case i of
+        CloseSession -> do
+          send $ Right $ Nothing
+        Keepalive -> do
+          send $ Right $ Nothing
+          loop
+        UserRequest a -> do
+          o <- liftIO $ processRequest state a
+          send $ Right $ Just o
+          loop
+  loop
+  where
+    receive = SessionT.receive :: SessionT m (Request i)
+    send = SessionT.send :: Response o -> SessionT m ()
 
-receive :: (Serializable IO i, Serializable IO o) => Session i o s (Interaction.Request i)
-receive = do
-  catchError (Session $ lift SesisonT.receive) $ \e -> do
-    case e of
-      SesisonT.TimeoutReached -> send $ Left $ Interaction.TimeoutReached
-      SesisonT.CorruptData t -> send $ Left $ Interaction.CorruptRequest t
-      _ -> return ()
-    throwError e
-
-send :: (Serializable IO o) => Interaction.Response o -> Session i o s ()
-send response = Session $ lift $ SesisonT.send response
-
-interact :: (Serializable IO i, Serializable IO o) => Session i o s ()
-interact = do
-  processMessage <- Session $ ask
-  receive >>= \case
-    Interaction.CloseSession -> do
-      send $ Right $ Nothing
-    Interaction.Keepalive -> do
-      send $ Right $ Nothing
-      interact
-    Interaction.UserRequest m -> do
-      reply <- liftIO $ processMessage m
-      send $ Right $ Just reply
-      interact
